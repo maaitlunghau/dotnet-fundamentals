c132213905a793c95531342b9ef9fdeca0f41108
maaitlunghau <chunhau.py@gmail.com>
2026-01-05 13:46:24 +0700

    chore: replace 01_demo with an empty project (01_gettingStarted)

--- 01_demo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _01_demo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ 01_gettingStarted/01_gettingStarted/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _01_gettingStarted.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
