6cd7b05d2b6b70f0373832316c8b16e2e98d0bed
maaitlunghau <chunhau.py@gmail.com>
2026-01-11 19:02:47 +0700

    chore: restructure lesson projects (7 lessons)

---
+++ 03_repository-pattern/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _03_repository_pattern.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
