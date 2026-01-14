1e7bb7c20c97f7cf63f7a168c60c4baabe2f7954
maaitlunghau <chunhau.py@gmail.com>
2026-01-14 18:42:30 +0700

    feat(lesson-10): initialize lesson 10 - dto (many to many)

---
+++ 10_dto_many-to-many/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _10_dto_many_to_many.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
