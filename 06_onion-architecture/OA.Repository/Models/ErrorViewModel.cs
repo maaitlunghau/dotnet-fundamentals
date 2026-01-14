1cabef6ce42f3d97b626b0e8303290fc29d6e47f
maaitlunghau <chunhau.py@gmail.com>
2026-01-14 10:34:50 +0700

    chore(lesson-06/architecture): initialize lesson project &  setup onion architecture layers

---
+++ 06_onion-architecture/OA.Repository/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OA.Repository.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ 06_onion-architecture/OA.Service/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OA.Service.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
