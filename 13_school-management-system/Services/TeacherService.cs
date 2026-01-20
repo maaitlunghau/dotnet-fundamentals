d08637af30de4aad0f5dbf0552f36a54aa758da5
maaitlunghau <chunhau.py@gmail.com>
2026-01-20 11:07:36 +0700

    feat(teacher): implement repository, service and controller

---
+++ 13_school-management-system/Services/TeacherService.cs
@@ -0,0 +1,43 @@
+using _13_school_management_system.Models;
+using _13_school_management_system.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace _13_school_management_system.Services;
+
+public class TeacherService : ITeacherRepository
+{
+    private readonly DataContext _dbContext;
+    public TeacherService(DataContext context) => _dbContext = context;
+
+    public async Task<IEnumerable<Teacher>> GetAllTeachersAsync()
+    {
+        return await _dbContext.Teachers.ToListAsync();
+    }
+
+    public async Task<Teacher?> GetTeacherByIdAsync(int? id)
+    {
+        return await _dbContext.Teachers.FindAsync(id);
+    }
+
+    public async Task CreateTeacherAsync(Teacher teacher)
+    {
+        await _dbContext.Teachers.AddAsync(teacher);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateTeacherAsync(Teacher teacher)
+    {
+        _dbContext.Teachers.Update(teacher);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteTeacherAsync(int? id)
+    {
+        var teacher = await GetTeacherByIdAsync(id);
+        if (teacher != null)
+        {
+            _dbContext.Teachers.Remove(teacher);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
--- 13_school-management-system/obj/Debug/net10.0/13_school-management-system.AssemblyInfo.cs
+++ 13_school-management-system/obj/Debug/net10.0/13_school-management-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9b3c0b91ff7f07e2d52c4101fcc287fae406f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22494516e135597f1bb98869cbde8e227bf91b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
