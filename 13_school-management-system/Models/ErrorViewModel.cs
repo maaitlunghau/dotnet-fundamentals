aa12a48f8af286190a771edd424252229887b991
maaitlunghau <chunhau.py@gmail.com>
2026-01-20 10:10:22 +0700

    refactor: re-intial ASP.NET MVC project (last time)

---
+++ 13_school-management-system/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _13_school_management_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
