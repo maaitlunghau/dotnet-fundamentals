4e1b383cc8223e8721bb9a5f3ab878ac58492b96
maaitlunghau <chunhau.py@gmail.com>
2026-01-21 22:03:58 +0700

    feat(teacher): handle view students feature in index

--- 13_school-management-system/obj/Debug/net10.0/13_school-management-system.AssemblyInfo.cs
+++ 13_school-management-system/obj/Debug/net10.0/13_school-management-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d1dd3fde7e4b291013a1b17ed01a7eaee51fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c761852e671d67fee5326829a5445ed9711f19a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("13_school-management-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
