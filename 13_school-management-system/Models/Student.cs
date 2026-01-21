b45e7f4c905fb1f016d6b93811b5809b9c7fbde1
maaitlunghau <chunhau.py@gmail.com>
2026-01-21 22:23:16 +0700

    feat(student): update validation

--- 13_school-management-system/Models/Student.cs
+++ 13_school-management-system/Models/Student.cs
@@ -13,17 +13,17 @@ public class Student
     public int TeacherId { get; set; }
 
     [Required(ErrorMessage = "Tên sinh viên không được phép bỏ trống!")]
-    [StringLength(60, ErrorMessage = "Tên sinh viên không được quá 60 kí tự!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "Tên sinh viên không được quá 60 kí tự!")]
     public string? StudentName { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Ngày sinh không được bỏ trống!")]
     [Column(TypeName = "date")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime? DateOfBirth { get; set; }
 
     [Required(ErrorMessage = "Địa chỉ sinh viên không được bỏ trống!")]
-    [StringLength(100, ErrorMessage = "Địa chỉ không được quá 100 kí tự!")]
+    [StringLength(100, MinimumLength = 10, ErrorMessage = "Địa chỉ phải từ 10 đến 100 kí tự!")]
     public string? Address { get; set; }
 
     public Teacher? Teacher { get; set; }
