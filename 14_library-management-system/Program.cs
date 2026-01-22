996ecde555be64063b00e72080d23ae4037eb738
maaitlunghau <chunhau.py@gmail.com>
2026-01-22 11:50:11 +0700

    feat(test-02/user): register DI container in Program.cs

--- 14_library-management-system/Program.cs
+++ 14_library-management-system/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectedDB"));
 });
 builder.Services.AddScoped<IBookRepository, BookService>();
+builder.Services.AddScoped<IAuthRepository, AuthService>();
+builder.Services.AddScoped<IUserRepository, UserService>();
 
 var app = builder.Build();
 
