6354fdf2a5186663e9dd453319f1cba1820ba0b7
maaitlunghau <chunhau.py@gmail.com>
2026-01-23 10:41:18 +0700

    feat(teacher): add interface repository, service and controller

---
+++ 13_school-management-system_practice/Controllers/TeacherController.cs
@@ -0,0 +1,55 @@
+using _13_school_management_system_practice.Models;
+using _13_school_management_system_practice.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _13_school_management_system_practice.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacherRepository _repo;
+        public TeacherController(ITeacherRepository repo) => _repo = repo;
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var teachers = await _repo.GetAllTeachersAsync();
+            return View(teachers);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Teacher? teacher)
+        {
+            if (!ModelState.IsValid) return View();
+
+            await _repo.CreateNewTeacherAsync(teacher);
+            TempData["message"] = "Thêm giảng viên mới thành công.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var teacher = await _repo.GetTeacherByIdAsync(id);
+            return View(teacher);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Teacher? teacher)
+        {
+            if (!ModelState.IsValid) return View();
+
+            await _repo.UpdateTeacherAsync(teacher);
+            TempData["message"] = "Cập nhật giảng viên thành công.";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ 13_school-management-system_practice/Services/TeacherService.cs
@@ -0,0 +1,41 @@
+using _13_school_management_system_practice.Models;
+using _13_school_management_system_practice.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace _13_school_management_system_practice.Services;
+
+public class TeacherService : ITeacherRepository
+{
+    private readonly DataContext _dbContext;
+    public TeacherService(DataContext dbContext) => _dbContext = dbContext;
+
+    public async Task<IEnumerable<Teacher>> GetAllTeachersAsync()
+    {
+        return await _dbContext.Teachers
+            .Include(t => t.Students)
+            .ToListAsync();
+    }
+
+    public async Task<Teacher?> GetTeacherByIdAsync(int? id)
+    {
+        return await _dbContext.Teachers.FindAsync(id);
+    }
+
+    public async Task CreateNewTeacherAsync(Teacher? t)
+    {
+        if (t == null)
+            throw new ArgumentNullException(nameof(t), "Teacher cannot be null");
+
+        await _dbContext.Teachers.AddAsync(t);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateTeacherAsync(Teacher? t)
+    {
+        if (t == null)
+            throw new ArgumentNullException(nameof(t), "Teacher cannot be null");
+
+        _dbContext.Teachers.Update(t);
+        await _dbContext.SaveChangesAsync();
+    }
+}
